85795701cb1d7beccd8370b891b597c7663a7d25
Chris Henry <henry.christopher@gmail.com>
2015-06-20 20:01:42 -0700

    Implement switching between local and docker hub

--- geonosis/main.go
+++ geonosis/main.go
@@ -28,6 +28,7 @@ type (
 type MyAPIImages struct {
 	ID          string    `json:"Id" yaml:"Id"`
 	RepoTag     string    `json:"RepoTags,omitempty" yaml:"RepoTags,omitempty"`
+	Source      string    `json:"Source,omitempty" yaml:"Source, omitempty"`
 	APIImages   dc.APIImages
 }
 
@@ -92,9 +93,11 @@ func deleteDeployment(c *echo.Context) error {
 	return c.String(http.StatusOK, "Deployment DELETE\n")
 }
 
-func getImage(c *echo.Context) error {
+func getLocalImage() []MyAPIImages {
 
 	docker := getDockerClient()
+
+	var returnimages []MyAPIImages
 
 	// Get local images
 	images, err := docker.ListImages(dc.ListImagesOptions{All: true})
@@ -102,24 +105,44 @@ func getImage(c *echo.Context) error {
 		log.Fatal(err)
 	}
 
-	var returnimages []MyAPIImages
-
 	for _, img := range images {
 
 		if img.RepoTags[0] != "<none>:<none>" {
 
 			for _, tag := range img.RepoTags {
 				fmt.Println("RepoTags: ", tag)
-				returnimages = append(returnimages, MyAPIImages{img.ID, tag, img})
+				returnimages = append(returnimages, MyAPIImages{img.ID, tag, "local", img})
 			}
 
 		}
 
 	}
 
+	return returnimages
+
+}
+
+func getImage(c *echo.Context) error {
+
+	r := c.Request()
+	var source string = ""
+	var returnimages []MyAPIImages
+
+	if len(r.URL.Query()["source"]) == 0 {
+		source = "local"
+	} else {
+		source = r.URL.Query()["source"][0]
+	}
+
+	fmt.Println("source: ", source)
+
+	if source == "local" {
+
+		returnimages = getLocalImage()
+
+	}
 
 	return c.JSON(http.StatusOK, returnimages)
-
 }
 
 func main() {
